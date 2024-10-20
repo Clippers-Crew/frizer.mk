858cbec667ffca296a2fde4ccebed3d269dadf8d
Dario <master15820@gmail.com>
2024-10-20 19:16:46 +0200

    Changed some styling in the SalonDetails page.

--- frizer-frontend/src/components/salonDetails/EmployeeItem/EmployeeItem.component.tsx
+++ frizer-frontend/src/components/salonDetails/EmployeeItem/EmployeeItem.component.tsx
@@ -1,35 +1,33 @@
-import React from 'react'
-import styles from './EmployeeItem.module.scss';
-import { Employee } from '../../../interfaces/Employee.interface';
-import { FaStar } from 'react-icons/fa6';
+import styles from "./EmployeeItem.module.scss";
+import { Employee } from "../../../interfaces/Employee.interface";
+import { FaStar } from "react-icons/fa6";
 interface EmployeeItemProps {
-    employee?: Employee
+  employee?: Employee;
 }
-function EmployeeItem({employee} :EmployeeItemProps) {
-    const formattedDate = employee?.employmentDate
+function EmployeeItem({ employee }: EmployeeItemProps) {
+  const formattedDate = employee?.employmentDate
     ? new Date(employee.employmentDate).getFullYear()
-    : '/';
+    : "/";
   return (
-    <div className= {`${styles.card}`}>
-              <img className= {`${styles.image}`}
-                src="/assets/salons/default_barber.jpg" 
-                alt="Barber image"
-              />
-            <div className= {`${styles.title}`}>
-              <h2>{employee?.firstName}</h2>
-            </div>
-            <p className= {`${styles.rating}`}>
-            <FaStar />
-            <span>
-                {employee?.rating.toFixed(2)}
-              </span>
-              <span>
-              ({employee?.numberOfReviews} рецензии)
-              </span>
-            </p>
-            <span>Работи од {formattedDate}</span>
-          </div>
-  )
+    <div className={styles.card}>
+      <img
+        className={styles.image}
+        src="/assets/salons/default_barber.jpg"
+        alt="Barber image"
+      />
+      <h2 className={`${styles.title}`}>{employee?.firstName}</h2>
+      <p className={`${styles.rating}`}>
+        <FaStar />
+        <span>
+            {employee?.rating.toPrecision(2)} { }
+            {employee && employee.numberOfReviews === 1
+              ? `(${employee?.numberOfReviews} рецензија)`
+              : `(${employee?.numberOfReviews} рецензии)`}
+          </span>
+      </p>
+      <span>Работи од {formattedDate}</span>
+    </div>
+  );
 }
 
 export default EmployeeItem;
